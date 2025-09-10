c158c58e655006ee6ec9f079994a3a3c255f9569
CagySloth <ngcl1013@gmail.com>
2025-09-10 15:49:33 +0800

    leaderboard api url route

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('players/', views.player_list, name='player-list'),
+    path('leaderboard/games/', views.top_players_by_games, name='top-players-by-games'),
 ]
